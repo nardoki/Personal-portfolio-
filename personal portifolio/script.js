a81ecd3e1571aae80029d8404052a125dcf39858
mihret habtamu <116177887+nardoki@users.noreply.github.com>
2024-06-29 20:54:37 +0300

    Update script.js

--- personal portifolio/script.js
+++ personal portifolio/script.js
@@ -1,8 +1,13 @@
- const scriptURL = 'https://script.google.com/macros/s/AKfycbxey9GsMNX0smsoD2j0YmJpNAPV51ZAI9TVHXfN2MCa7u0-bPIEYwi7VkvX03UBMyh1/exec'
-  const form = document.forms['submit-to-google-sheet']
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+const scriptURL = "https://script.google.com/macros/s/AKfycbxey9GsMNX0smsoD2j0YmJpNAPV51ZAI9TVHXfN2MCa7u0-bPIEYwi7VkvX03UBMyh1/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => { msg.innerHTML = "Message sent successfully";})
+      .catch(error => console.error("Error!", error.message));
+      setTimeout(function () {
+        msg.innerHTML = "";
+       }, 2500);
+       form.reset();
+       });
